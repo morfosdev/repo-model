44e410e8503080b50a4b84e10e6a24a8cf4a90e5
Carlos A. <carlosvsk@gmail.com>
2024-07-08 19:05:44 +0000

    fix: new github flaxboll structure

--- .flaxboll/seeds/base/system/index.ts
+++
@@ -1,8 +0,0 @@
-import { TcapsCodeData } from '../../../types/Tcaps/TcapsCode';
-import { ccc_dirSeed } from './ccc_dirSeed';
-import { ccc_fileSeed } from './ccc_fileSeed';
-
-export const system: TcapsCodeData = {
-  ...ccc_dirSeed,
-  ...ccc_fileSeed,
-};
--- .flaxboll/types/utils.ts
+++
@@ -1,4 +0,0 @@
-export type TobjKeys<T> = { [key in keyof T]: T[keyof T] };
-
-export type TObjProps<T> = { [key: string]: T };
-export type TobjAny = TObjProps<any>;
